9a5b7de89662678dcb8b4501b7d79535de463c3b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-09-18 16:28:08 -0400

    fix: `delete` TS error

--- index.ts
+++ index.ts
@@ -43,7 +43,7 @@ function setDisplayNameIfPossible(
     )
   )
   newStmt.trailingComments = prevStmt.node.trailingComments
-  delete prevStmt.node.trailingComments
+  prevStmt.node.trailingComments = null
   prevStmt.insertAfter(newStmt)
 }
 
